7ae0246371f5179b7fbdd1d1d936b738ff780da0
xiaolei hu <huxiaolei1997@gmail.com>
2018-09-27 21:19:49 +0800

    更新配置文件

--- src/main/java/com/example/client/ClientApplication.java
+++ src/main/java/com/example/client/ClientApplication.java
@@ -2,10 +2,10 @@ package com.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient // 如果注册中心是 eureka，那么首选 @EnableEurekaClient，反之则选 @EnableDiscoveryClient
 public class ClientApplication {
 
     public static void main(String[] args) {
